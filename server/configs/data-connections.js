fb20f0dc07ae3f586b1aa65a4c6b40a02c0774b6
Nick Webster <nick.webster@qlikview.com>
2016-07-06 15:36:28 +0200

    removed redundant configs

--- server/configs/data-connections.js
+++ server/configs/data-connections.js
@@ -1,16 +1,4 @@
 module.exports = {
-  untappd:{
-    id: "untappd",
-    name: "Untappd",
-    directory: "untappd-dictionary-master",
-    endpoint: "https://api.untappd.com",
-    description: "Contains your checked in beers, badges and basic brewery info.",
-    dictionary: "/dictionaries/untappd/dictionary.json",
-    icon: "/dictionaries/untappd/icon.png",
-    loadscript: "",
-    clientId: "",
-    clientSecret: ""
-  },
   github:{
     id: "github",
     name: "GitHub",
@@ -19,7 +7,7 @@ module.exports = {
     description: "All your GitHub repos, issues and stars.",
     dictionary: "/dictionaries/github/dictionary.json",
     icon: "/dictionaries/github/icon.png",
-    loadscript: "Users:LOAD *; SQL SELECT * FROM User;Repos:LOAD *; SQL SELECT * FROM Repos;",    
+    loadscript: "Users:LOAD *; SQL SELECT * FROM User;Repos:LOAD *; SQL SELECT * FROM Repos;",
     projects: [
       "basic-template",
       "github-search-template"
