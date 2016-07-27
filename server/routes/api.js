f085ef5be89e1bbd68ef6a074eeaff22ddbec847
Nick Webster <nick.webster@qlikview.com>
2016-07-27 19:04:24 +0100

    switched ticket logic to fake a login to the hub and return the session cookie

--- server/routes/api.js
+++ server/routes/api.js
@@ -6,7 +6,7 @@ var express = require('express'),
     mongoHelper = require('../controllers/mongo-helper'),
     generalConfig = require('../configs/general'),
     OAuthInfo = require('../configs/oauth-info'),
-    Cookie = require('cookie'),
+    Cookies = require('cookies'),
     cookieParser = require('cookie-parser'),
     QRS = require('../controllers/qrs');
 
@@ -20,53 +20,13 @@ router.get('/ticket', function(req, res){
       res.json({err: err});
     }
     else{
+      var sessCookie = cookies.get(process.env.sessionCookieName);
+      if(sessCookie){
+        cookies.set(process.env.sessionCookieName, sessCookie, {httpOnly:false});
+      }
+      console.log(cookies);
       res.setHeader('Set-Cookie', cookies);
       res.send(JSON.stringify({}));
-    }
-  });
-});
-
-router.get('/getAppInfoX', function(req, res){
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Content-Type", "application/json");
-  QRS.checkOrCreateSession(req, function(err, sessionResponse){
-    console.log(sessionResponse);
-    if(err){
-      res.send(JSON.stringify({err:err}));
-    }
-    else{
-      console.log(req.session);
-      console.log('setting cookie header');
-      var cookies = [];
-      for (var c in sessionResponse.cookies){
-        if(c==process.env.sessionCookieName){
-          cookies.push(Cookie.serialize(c,sessionResponse.cookies[c], {httpOnly: false}));
-        }
-        else{
-          cookies.push(Cookie.serialize(c,sessionResponse.cookies[c]));
-        }
-      }
-
-      // res.setHeader('Set-Cookie', cookies.join());
-      // var sessCookie = Cookie.serialize(process.env.sessionCookieName, sessionResponse.session.SessionId, {domain: process.env.cookieDomain, httpOnly: false});
-      // res.cookie(sessCookie);
-      mongoHelper.getConnectionString(sessionResponse.session.origUserId, req.query.app, function(err, connectionStrings){
-        if(err){
-          res.send(JSON.stringify({err:err}));
-        }
-        else if (connectionStrings.length==0) {
-          res.send(JSON.stringify({err: "No connection found"}));
-        }
-        else{
-          var info = generalConfig;
-          if(sessionResponse.session.Ticket){
-            generalConfig.ticket = sessionResponse.session.Ticket;
-          }
-          generalConfig.connectionString = connectionStrings[0].connectionString;
-          generalConfig.loadscript = dataConnections[req.query.app].loadscript;
-          res.send(JSON.stringify(generalConfig));
-        }
-      });
     }
   });
 });
