cceb6e5cb30fd139464e9b9ddcc21faefcca39c0
brianwmunz <brian.munz@qlik.com>
2016-09-13 12:41:00 -0400

    reverting

--- server/configs/showcase-projects.js
+++ server/configs/showcase-projects.js
@@ -2,7 +2,7 @@ module.exports = {
   untappd: {
     id: "untappd",
     name: "Untappd Dashboard",
-    description: "An example dashboard that uses data from Untappd",
+    description: "An example dashboard that uses data from the Untappd mobile app",
     url: "/liveshowcase/untappd/index.html",
     ownData: true,
     connectionId: "untappd",
