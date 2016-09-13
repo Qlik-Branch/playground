7948a69e8512084e01152792ad899059d569bca6
brianwmunz <brian.munz@qlik.com>
2016-09-13 12:34:03 -0400

    added to showcase list

--- server/configs/showcase-projects.js
+++ server/configs/showcase-projects.js
@@ -9,5 +9,16 @@ module.exports = {
     sampleProjectId: "untappd-capability-dashboard",
     icon: "/dictionaries/untappd/icon.png",
     api: "Capability APIs"
+  },
+   airBnB-app: {
+    id: "airBnB-app",
+    name: "AirBnB Web Application",
+    description: "An example application using data from AirBnB",
+    url: "/liveshowcase/airBnB-app/index.html",
+    ownData: true,
+    connectionId: "airBnB-app",
+    sampleProjectId: "airBnB-app",
+    icon: "/resources/airBnB-logo.png",
+    api: "Capability APIs"
   }
 }
